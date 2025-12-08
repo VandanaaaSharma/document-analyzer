2acaba7c69f007401e93b7817f7fcb1e5ecf14f6
VandanaaaSharma <vandanasharmajewar007@gmail.com>
2025-12-08 22:12:07 +0530

    done

--- src/main/java/com/docai/document_analyzer/util/PdfUtil.java
+++ src/main/java/com/docai/document_analyzer/util/PdfUtil.java
@@ -1,5 +1,16 @@
 package com.docai.document_analyzer.util;
 
+import org.apache.tika.Tika;
+import org.springframework.stereotype.Component;
+
+import java.io.InputStream;
+
+@Component
 public class PdfUtil {
-    
+
+    private final Tika tika = new Tika();
+
+    public String extractText(InputStream inputStream) throws Exception {
+        return tika.parseToString(inputStream);
+    }
 }
