3a76e0a40163f4d34dec442787f3fa6c6cd9ed20
VandanaaaSharma <vandanasharmajewar007@gmail.com>
2025-12-08 22:13:27 +0530

    done

--- src/main/java/com/docai/document_analyzer/util/DocxUtil.java
+++ src/main/java/com/docai/document_analyzer/util/DocxUtil.java
@@ -1,5 +1,17 @@
 package com.docai.document_analyzer.util;
 
+import org.apache.tika.Tika;
+import org.springframework.stereotype.Component;
+import java.io.InputStream;
+
+@Component
 public class DocxUtil {
-    
+
+    private final Tika tika = new Tika();
+
+    public String extractText(InputStream inputStream) throws Exception {
+        return tika.parseToString(inputStream);
+    }
 }
+ 
+
