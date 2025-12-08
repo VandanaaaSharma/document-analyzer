6e21c7f029f528dd66093224df900229619e00eb
VandanaaaSharma <vandanasharmajewar007@gmail.com>
2025-12-08 21:58:45 +0530

    done

---
+++ src/main/java/com/docai/document_analyzer/DocumentAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.docai.document_analyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocumentAnalyzerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/docai/document_analyzer/DocumentAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.docai.document_analyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
