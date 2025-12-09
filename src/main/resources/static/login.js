76d844ec31f44846883a55ee8fbc544e0df6a305
VandanaaaSharma <vandanasharmajewar007@gmail.com>
2025-12-09 12:17:06 +0530

    done

--- src/main/resources/static/login.js
+++ src/main/resources/static/login.js
@@ -1,11 +1,11 @@
-function login() {
-    const user = document.getElementById("username").value;
-    const pass = document.getElementById("password").value;
+// function login() {
+//     const user = document.getElementById("username").value;
+//     const pass = document.getElementById("password").value;
 
-    if (user === "admin" && pass === "admin123") {
-        window.location.href = "index.html";
-    } else {
-        document.getElementById("loginError").textContent = "Invalid login!";
-    }
-}
+//     if (user === "admin" && pass === "admin123") {
+//         window.location.href = "index.html";
+//     } else {
+//         document.getElementById("loginError").textContent = "Invalid login!";
+//     }
+// }
 
